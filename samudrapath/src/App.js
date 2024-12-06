25aaee996dea2ab20b97d993cade34dddcef951d
swapavan11 <swapnil.sontakke3s@gmail.com>
2024-12-06 20:13:07 +0530

    Added App.js

--- samudrapath/src/App.js
+++ samudrapath/src/App.js
@@ -1,1 +1,17 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";  
+import ShipHealth from "./Pages/ShipHealth";
+import HomePage from "./Pages/HomePage";
 
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage/> }/>
+        <Route path="/shiphealth" element={<ShipHealth />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
